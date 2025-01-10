8b018fc7740f67984d4a7463dc9ff981c3e1d35c
Liam Seys <liam.seys@gmail.com>
2025-01-10 22:51:28 +0100

    docs: update project name in disclaimer view

--- resources/views/disclaimer.blade.php
+++ resources/views/disclaimer.blade.php
@@ -1,7 +1,7 @@
 <div class="py-8 px-6">
     <div class="p-4 bg-gray-100 rounded-lg">
         <p class="text-gray-500 text-xs">
-            This project, {{ config('app.name') }}, is free and open source, built by Liam Seys.
+            This project, Eagle, is free and open source, built by Liam Seys.
             Consider contributing or sponsoring to support its development.
         </p>
     </div>
