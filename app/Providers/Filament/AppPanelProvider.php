f7bf12f27870e6d2589c9dbeb43c5181fc955edf
Liam Seys <liam.seys@gmail.com>
2025-01-10 18:04:20 +0100

    feat: add brand logo height to AppPanelProvider

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -31,6 +31,7 @@ class AppPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->brandLogo(asset('img/logo/logo-black.svg'))
+            ->brandLogoHeight('2rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
