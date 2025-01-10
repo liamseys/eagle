526904af9911192a86de86be270d3a7527329be2
Liam Seys <liam.seys@gmail.com>
2025-01-10 18:32:22 +0100

    feat: add Vite theme configuration for Filament panel

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -34,6 +34,7 @@ class AppPanelProvider extends PanelProvider
             ->brandLogoHeight('2rem')
             ->favicon(asset('favicon.png'))
             ->darkMode(false)
+            ->viteTheme('resources/css/filament/app/theme.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
