579251f00dbb75fb4a75f7eb82bf24c0d5f9259f
Liam Seys <liam.seys@gmail.com>
2025-01-11 00:07:38 +0100

    refactor(FormResource): remove unused imports and clean up code

--- app/Filament/Resources/FormResource.php
+++ app/Filament/Resources/FormResource.php
@@ -3,15 +3,12 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\FormResource\Pages;
-use App\Filament\Resources\FormResource\RelationManagers;
 use App\Models\Form as FormModel;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class FormResource extends Resource
 {
@@ -39,7 +36,7 @@ class FormResource extends Resource
                             ->required(),
                         Forms\Components\Toggle::make('is_active')
                             ->required(),
-                    ])
+                    ]),
                 ])->columnSpan(1),
             ])->columns(3);
     }
