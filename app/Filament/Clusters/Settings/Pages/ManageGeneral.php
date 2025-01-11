56d776a7a7361a626cf9bbac5f559554fb78b6a9
Liam Seys <liam.seys@gmail.com>
2025-01-11 13:39:19 +0100

    refactor(Settings): update navigation icon and labels

--- app/Filament/Clusters/Settings/Pages/ManageGeneral.php
+++ app/Filament/Clusters/Settings/Pages/ManageGeneral.php
@@ -4,13 +4,18 @@ namespace App\Filament\Clusters\Settings\Pages;
 
 use App\Filament\Clusters\Settings;
 use App\Settings\GeneralSettings;
-use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Pages\SettingsPage;
 
 class ManageGeneral extends SettingsPage
 {
-    protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
+    protected static ?string $navigationIcon = 'heroicon-o-adjustments-horizontal';
+
+    protected static ?string $navigationLabel = 'General';
+
+    protected static ?string $slug = 'general';
+
+    protected ?string $heading = 'General Settings';
 
     protected static string $settings = GeneralSettings::class;
 
