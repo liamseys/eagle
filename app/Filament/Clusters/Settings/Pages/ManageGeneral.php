91d52e37a3d4c084c4976be2d76a359dbc84a62d
Liam Seys <liam.seys@gmail.com>
2025-01-11 13:23:43 +0100

    feat: add ManageGeneral settings page class

---
+++ app/Filament/Clusters/Settings/Pages/ManageGeneral.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Clusters\Settings\Pages;
+
+use App\Filament\Clusters\Settings;
+use App\Settings\GeneralSettings;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Pages\SettingsPage;
+
+class ManageGeneral extends SettingsPage
+{
+    protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
+
+    protected static string $settings = GeneralSettings::class;
+
+    protected static ?string $cluster = Settings::class;
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                // ...
+            ]);
+    }
+}
