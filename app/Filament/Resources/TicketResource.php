0ce1d98aef1d1c8042f9835e1b2455a43cbe1e2d
Liam Seys <liam.seys@gmail.com>
2025-01-13 18:13:43 +0100

    feat: add navigation sort for TicketResource

--- app/Filament/Resources/TicketResource.php
+++ app/Filament/Resources/TicketResource.php
@@ -14,6 +14,8 @@ class TicketResource extends Resource
 {
     protected static ?string $model = Ticket::class;
 
+    protected static ?int $navigationSort = 1;
+
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
