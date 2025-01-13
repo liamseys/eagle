3c9a2374ae870d225647d376bb831b0c4e64e1af
Liam Seys <liam.seys@gmail.com>
2025-01-13 17:56:33 +0100

    refactor: update navigationSort values for resources

--- app/Filament/Clusters/Settings.php
+++ app/Filament/Clusters/Settings.php
@@ -6,7 +6,7 @@ use Filament\Clusters\Cluster;
 
 class Settings extends Cluster
 {
-    protected static ?int $navigationSort = 2;
+    protected static ?int $navigationSort = 3;
 
     protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
 }
--- app/Filament/Resources/TicketResource.php
+++ app/Filament/Resources/TicketResource.php
@@ -17,7 +17,9 @@ class TicketResource extends Resource
 {
     protected static ?string $model = Ticket::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?int $navigationSort = 1;
+
+    protected static ?string $navigationIcon = 'heroicon-o-ticket';
 
     public static function form(Form $form): Form
     {
