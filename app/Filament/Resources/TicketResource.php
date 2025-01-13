82885302d0fc2c3c6d408faa4a529a15525e2c91
Liam Seys <liam.seys@gmail.com>
2025-01-13 18:13:57 +0100

    style: update navigation icon for TicketResource

--- app/Filament/Resources/TicketResource.php
+++ app/Filament/Resources/TicketResource.php
@@ -16,7 +16,7 @@ class TicketResource extends Resource
 
     protected static ?int $navigationSort = 1;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-ticket';
 
     public static function form(Form $form): Form
     {
