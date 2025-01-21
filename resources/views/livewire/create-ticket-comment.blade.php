8a5822416f4559b57d065618e71c95a6e7d734b8
Liam Seys <liam.seys@gmail.com>
2025-01-21 22:11:48 +0100

    style: Improve layout and structure of comment form

--- resources/views/livewire/create-ticket-comment.blade.php
+++ resources/views/livewire/create-ticket-comment.blade.php
@@ -1,11 +1,18 @@
 <div>
-    <form>
-        {{ $this->form }}
+    <x-filament::section>
+        <form>
+            {{ $this->form }}
 
-        <x-filament::button wire:click="create">
-            {{ __('Submit') }}
-        </x-filament::button>
-    </form>
+            <x-slot name="footerActions">
+                <x-filament::button wire:click="create">
+                    <div class="flex items-center gap-1">
+                        <x-heroicon-s-paper-airplane class="h-4 w-4"/>
+                        {{ __('Send') }}
+                    </div>
+                </x-filament::button>
+            </x-slot>
+        </form>
+    </x-filament::section>
 
     <x-filament-actions::modals />
 </div>
