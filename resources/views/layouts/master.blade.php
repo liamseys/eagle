d9a386ec950e35d8d0287402afcad4d284e3f246
Liam Seys <liam.seys@gmail.com>
2025-01-22 16:17:06 +0100

    refactor(layout): simplify body class handling in layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        //
+    <body {{ $attributes->merge(['class' => 'font-sans text-gray-900 antialiased']) }}>
+        {{ $slot }}
     </body>
 </html>
