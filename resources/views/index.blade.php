e129838220dc59ed4d3af622b8b7a12f0e4d78e8
Liam Seys <liam.seys@gmail.com>
2025-01-23 17:32:29 +0100

    feat: add Help Center section and update font family

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,14 @@
     </header>
 
     <main>
-        //
+        <section class="py-12 bg-gradient-to-r from-[#F8CB09] via-[#EB2622] to-[#7506BF]">
+            <x-container>
+                <div class="flex flex-col gap-2">
+                    <h1 class="text-3xl font-bold text-white">{{ __('Help Center') }}</h1>
+                    <p class="w-full lg:w-1/2 text-white">{{ __('Here you\'ll find answers to many common questions about our services, policies, and features. If you need further assistance, our team is always ready to help you find the information or support you need.') }}</p>
+                </div>
+            </x-container>
+        </section>
     </main>
 
     <footer>
