89422642a69d1ec2b5900b700901d993fafd8de9
Liam Seys <liam.seys@gmail.com>
2025-01-23 16:46:52 +0100

    feat: add container component to index layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,12 @@
 <x-master-layout class="bg-white">
-    <header>
-        //
+    <header class="bg-black">
+        <x-container>
+            <div class="relative flex h-16 items-center justify-between">
+                <img src="{{ asset('img/logo/logo-white.svg') }}"
+                     alt="{{ config('app.name') }}"
+                     class="h-8">
+            </div>
+        </x-container>
     </header>
 
     <main>
