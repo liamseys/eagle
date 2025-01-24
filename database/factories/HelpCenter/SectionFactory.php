3852a3eca7722db2adb314f9fa0066eee0f78fd6
Liam Seys <liam.seys@gmail.com>
2025-01-24 14:19:01 +0100

    feat(database): add SectionFactory for HelpCenter model

---
+++ database/factories/HelpCenter/SectionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories\HelpCenter;
+
+use App\Models\HelpCenter\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\HelpCenter\Section>
+ */
+class SectionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id' => Category::factory(),
+            'name' => fake()->word(),
+        ];
+    }
+}
