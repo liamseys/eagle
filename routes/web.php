993a1fc548d0bc962d8c0b5c2a71cc5228db1219
Liam Seys <liam.seys@gmail.com>
2025-01-24 17:39:46 +0100

    feat(routes): add locale middleware for routing logic

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Middleware\SetDefaultLocaleForUrls;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', IndexController::class);
+Route::get('/', function () {
+    return redirect()->route('index');
+})->middleware(SetDefaultLocaleForUrls::class);
+
+Route::group([
+    'prefix' => 'hc/{locale}',
+    'where' => ['locale' => '[a-zA-Z]{2}'],
+    'middleware' => SetDefaultLocaleForUrls::class,
+], function () {
+    Route::get('/', IndexController::class)->name('index');
+});
