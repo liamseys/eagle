18ea8697cbad7773ef5b4bac642ed3fa5ba3bf79
Liam Seys <liam.seys@gmail.com>
2025-01-24 23:11:49 +0100

    feat: update layout for Help Center page and app layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,48 +1,32 @@
-<x-master-layout class="bg-white">
-    <header class="bg-black">
+<x-app-layout>
+    <section class="py-12 bg-gradient-to-r from-[#F8CB09] via-[#EB2622] to-[#7506BF]">
         <x-container>
-            <div class="relative flex h-16 items-center justify-between">
-                <img src="{{ asset('img/logo/logo-white.svg') }}"
-                     alt="{{ config('app.name') }}"
-                     class="h-8">
+            <div class="flex flex-col gap-2">
+                <h1 class="text-3xl font-bold text-white">{{ __('Help Center') }}</h1>
+                <p class="w-full lg:w-1/2 text-sm text-white">
+                    {{ __('Here you\'ll find answers to many common questions about our services, policies, and features. If you need further assistance, our team is always ready to help you find the information or support you need.') }}
+                </p>
             </div>
         </x-container>
-    </header>
+    </section>
 
-    <main>
-        <section class="py-12 bg-gradient-to-r from-[#F8CB09] via-[#EB2622] to-[#7506BF]">
-            <x-container>
-                <div class="flex flex-col gap-2">
-                    <h1 class="text-3xl font-bold text-white">{{ __('Help Center') }}</h1>
-                    <p class="w-full lg:w-1/2 text-sm text-white">
-                        {{ __('Here you\'ll find answers to many common questions about our services, policies, and features. If you need further assistance, our team is always ready to help you find the information or support you need.') }}
-                    </p>
-                </div>
-            </x-container>
-        </section>
+    <section class="py-12">
+        <x-container>
+            <div class="grid gap-4 grid-cols-1 sm:grid-cols-3">
+                @foreach($categories as $category)
+                    <a href="{{ route('categories.show', $category) }}">
+                        <div class="flex flex-col p-4 space-y-2 border rounded-lg hover:bg-gray-100 hover:cursor-pointer">
+                            <x-dynamic-component :component="$category->icon" class="h-6 w-6"/>
 
-        <section class="py-12">
-            <x-container>
-                <div class="grid gap-4 grid-cols-1 sm:grid-cols-3">
-                    @foreach($categories as $category)
-                        <a href="{{ route('categories.show', $category) }}">
-                            <div class="flex flex-col p-4 space-y-2 border rounded-lg hover:bg-gray-100 hover:cursor-pointer">
-                                <x-dynamic-component :component="$category->icon" class="h-6 w-6"/>
+                            <p class="font-bold text-[#F8CB09]">{{ $category->name }}</p>
 
-                                <p class="font-bold text-[#F8CB09]">{{ $category->name }}</p>
-
-                                @if($category->description)
-                                    <p class="text-sm text-gray-500">{{ $category->description }}</p>
-                                @endif
-                            </div>
-                        </a>
-                    @endforeach
-                </div>
-            </x-container>
-        </section>
-    </main>
-
-    <footer>
-        //
-    </footer>
-</x-master-layout>
+                            @if($category->description)
+                                <p class="text-sm text-gray-500">{{ $category->description }}</p>
+                            @endif
+                        </div>
+                    </a>
+                @endforeach
+            </div>
+        </x-container>
+    </section>
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<x-master-layout class="bg-white">
+    <header class="bg-black">
+        <x-container>
+            <div class="relative flex h-16 items-center justify-between">
+                <img src="{{ asset('img/logo/logo-white.svg') }}"
+                     alt="{{ config('app.name') }}"
+                     class="h-8">
+            </div>
+        </x-container>
+    </header>
+
+    <main>
+        {{ $slot }}
+    </main>
+
+    <footer>
+        //
+    </footer>
+</x-master-layout>
