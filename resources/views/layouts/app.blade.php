7fd448c3449af972b5abda0e34eb2d582a719287
Liam Seys <liam.seys@gmail.com>
2025-01-24 23:21:05 +0100

    feat: wrap logo in link for homepage navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,11 @@
     <header class="bg-black">
         <x-container>
             <div class="relative flex h-16 items-center justify-between">
-                <img src="{{ asset('img/logo/logo-white.svg') }}"
-                     alt="{{ config('app.name') }}"
-                     class="h-8">
+                <a href="{{ route('index') }}">
+                    <img src="{{ asset('img/logo/logo-white.svg') }}"
+                         alt="{{ config('app.name') }}"
+                         class="h-8">
+                </a>
             </div>
         </x-container>
     </header>
