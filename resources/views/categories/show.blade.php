db7f1d44500f681d5d1b87548a1ff22feb4448c1
Liam Seys <liam.seys@gmail.com>
2025-01-24 23:20:00 +0100

    feat: add Help Center category view blade template

---
+++ resources/views/categories/show.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <section class="py-12 bg-gradient-to-r from-[#F8CB09] via-[#EB2622] to-[#7506BF]">
+        <x-container>
+            <div class="flex flex-col gap-2">
+                <h1 class="text-3xl font-bold text-white">{{ __('Help Center') }}</h1>
+            </div>
+        </x-container>
+    </section>
+
+    <section class="py-12">
+        <x-container>
+            //
+        </x-container>
+    </section>
+</x-app-layout>
