4ca7632ea108ad2efc7211f594fda6e1b935b886
Liam Seys <liam.seys@gmail.com>
2025-01-25 12:14:31 +0100

    feat: enhance article display with new styling and layout

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -3,7 +3,22 @@
 
     <section class="py-12">
         <x-container>
-            <h2 class="text-xl font-semibold">{{ $article->title }}</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                <div class="col-span-2">
+                    <x-card>
+                        <x-slot name="header">
+                            <div class="flex flex-col gap-2">
+                                <h2 class="text-xl font-semibold">{{ $article->title }}</h2>
+                                <p class="text-sm text-gray-500">{{ $article->description }}</p>
+                            </div>
+                        </x-slot>
+
+                        <div class="article-content">
+                            {!! $article->body !!}
+                        </div>
+                    </x-card>
+                </div>
+            </div>
         </x-container>
     </section>
 </x-app-layout>
