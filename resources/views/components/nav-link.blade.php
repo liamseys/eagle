ba0b7428c48954648ad12e1026f3bcb3d954d066
Liam Seys <liam.seys@gmail.com>
2025-01-26 21:34:34 +0100

    style(nav-link): update background color in nav link component

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
     $classes = ($active ?? false)
-                ? 'flex items-center justify-between p-4 rounded-lg bg-yellow-50 text-yellow-500'
+                ? 'flex items-center justify-between p-4 rounded-lg bg-gray-100'
                 : 'flex items-center justify-between p-4 rounded-lg hover:bg-gray-100';
 @endphp
 
