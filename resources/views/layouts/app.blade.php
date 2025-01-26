816657806af307de82f72d4d2d5b17d5ac4cb30a
Liam Seys <liam.seys@gmail.com>
2025-01-26 20:57:27 +0100

    style: Update header link with logo and help center label

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,13 @@
     <header class="bg-black">
         <x-container>
             <div class="relative flex h-16 items-center justify-between">
-                <a href="{{ route('index') }}">
+                <a href="{{ route('index') }}" class="flex items-center gap-2">
                     <img src="{{ asset('img/logo/logo-white.svg') }}"
                          alt="{{ config('app.name') }}"
                          class="h-8">
+                    <span class="px-2 py-1 text-xs font-bold bg-white rounded-lg">
+                        {{ __('Help Center') }}
+                    </span>
                 </a>
             </div>
         </x-container>
