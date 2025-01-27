107cf5a5f7b7d7586717a053032d95cda5d7d447
Liam Seys <liam.seys@gmail.com>
2025-01-27 23:38:31 +0100

    feat(breadcrumbs): add breadcrumbs functionality and config

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,8 @@
+<?php
+
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+Breadcrumbs::for('index', function (BreadcrumbTrail $trail) {
+    $trail->push(__('Help Center'), route('index'));
+});
