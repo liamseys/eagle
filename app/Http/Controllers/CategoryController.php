ab41863f998b8a209fd9007e8784c6854a563779
Liam Seys <liam.seys@gmail.com>
2025-01-27 22:42:00 +0100

    feat(CategoryController): add sections filtering by status

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\HelpCenter\ArticleStatus;
 use App\Models\HelpCenter\Category;
 
 class CategoryController extends Controller
@@ -11,9 +12,15 @@ class CategoryController extends Controller
      */
     public function show($locale, Category $category)
     {
+        $sections = $category->sections()->whereHas('articles', function ($query) {
+            $query->where('status', ArticleStatus::PUBLISHED)
+                ->where('is_public', '=', true);
+        })->get();
+
         return view('categories.show', [
             'locale' => $locale,
             'category' => $category,
+            'sections' => $sections,
         ]);
     }
 }
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -6,7 +6,7 @@
             <h2 class="text-xl font-semibold">{{ $category->name }}</h2>
 
             <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-6">
-                @foreach($category->sections as $section)
+                @foreach($sections as $section)
                     <x-card>
                         <x-slot name="header">
                             <h3 class="font-semibold">{{ $section->name }}</h3>
@@ -14,8 +14,8 @@
 
                         <ul class="-mx-4">
                             @foreach($section->articles()
-                                             ->public()
                                              ->published()
+                                             ->public()
                                              ->get() as $article)
                                 <x-nav-link :href="route('articles.show', $article)">
                                     <p class="text-sm">{{ $article->title }}</p>
