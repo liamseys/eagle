c8dae7e0c412fb483bd66e684fb795a828d32580
Liam Seys <liam.seys@gmail.com>
2025-01-28 10:15:48 +0100

    feat: add handling for empty categories in view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,21 +4,28 @@
 
     <section class="py-12">
         <x-container class="max-w-7xl">
-            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                @foreach($categories as $category)
-                    <a href="{{ route('categories.show', $category) }}">
-                        <div class="flex flex-col p-4 space-y-2 border rounded-lg hover:bg-gray-100 hover:cursor-pointer">
-                            <x-dynamic-component :component="$category->icon" class="h-6 w-6"/>
+            @if(!$categories->isEmpty())
+                <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                    @foreach($categories as $category)
+                        <a href="{{ route('categories.show', $category) }}">
+                            <div class="flex flex-col p-4 space-y-2 border rounded-lg hover:bg-gray-100 hover:cursor-pointer">
+                                <x-dynamic-component :component="$category->icon" class="h-6 w-6"/>
 
-                            <p class="font-bold text-[#F8CB09]">{{ $category->name }}</p>
+                                <p class="font-bold text-[#F8CB09]">{{ $category->name }}</p>
 
-                            @if($category->description)
-                                <p class="text-sm text-gray-500">{{ $category->description }}</p>
-                            @endif
-                        </div>
-                    </a>
-                @endforeach
-            </div>
+                                @if($category->description)
+                                    <p class="text-sm text-gray-500">{{ $category->description }}</p>
+                                @endif
+                            </div>
+                        </a>
+                    @endforeach
+                </div>
+            @else
+                <div class="text-center flex flex-col gap-6 mt-6">
+                    <img src="{{ asset('img/no_results.svg') }}" alt="No categories" class="h-24 mx-auto">
+                    <p class="text-gray-500">{{ __('No categories found.') }}</p>
+                </div>
+            @endif
         </x-container>
     </section>
 </x-app-layout>
