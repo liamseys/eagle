43c95e5b589ad894e2b29bc28f90fea3e9b3fc9c
Liam Seys <liam.seys@gmail.com>
2025-01-28 10:13:58 +0100

    feat: add conditional display for empty sections in view

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -5,27 +5,34 @@
         <x-container class="max-w-7xl">
             <h2 class="text-xl font-semibold">{{ $category->name }}</h2>
 
-            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-6">
-                @foreach($sections as $section)
-                    <x-card>
-                        <x-slot name="header">
-                            <h3 class="font-semibold">{{ $section->name }}</h3>
-                        </x-slot>
+            @if(!$sections->isEmpty())
+                <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-6">
+                    @foreach($sections as $section)
+                        <x-card>
+                            <x-slot name="header">
+                                <h3 class="font-semibold">{{ $section->name }}</h3>
+                            </x-slot>
 
-                        <ul class="-mx-4">
-                            @foreach($section->articles()
-                                             ->published()
-                                             ->public()
-                                             ->get() as $article)
-                                <x-nav-link :href="route('articles.show', $article)">
-                                    <p class="text-sm">{{ $article->title }}</p>
-                                    <x-heroicon-s-chevron-right class="h-4 w-4"/>
-                                </x-nav-link>
-                            @endforeach
-                        </ul>
-                    </x-card>
-                @endforeach
-            </div>
+                            <ul class="-mx-4">
+                                @foreach($section->articles()
+                                                 ->published()
+                                                 ->public()
+                                                 ->get() as $article)
+                                    <x-nav-link :href="route('articles.show', $article)">
+                                        <p class="text-sm">{{ $article->title }}</p>
+                                        <x-heroicon-s-chevron-right class="h-4 w-4"/>
+                                    </x-nav-link>
+                                @endforeach
+                            </ul>
+                        </x-card>
+                    @endforeach
+                </div>
+            @else
+                <div class="text-center flex flex-col gap-6 mt-6">
+                    <img src="{{ asset('img/no_results.svg') }}" alt="No sections" class="h-24 mx-auto">
+                    <p class="text-gray-500">{{ __('No sections found.') }}</p>
+                </div>
+            @endif
         </x-container>
     </section>
 </x-app-layout>
