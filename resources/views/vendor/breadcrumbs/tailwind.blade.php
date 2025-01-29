097edcc6b58971dd6f2a366a918be1a1e8f1d21e
Liam Seys <liam.seys@gmail.com>
2025-01-29 17:45:17 +0100

    style(breadcrumbs): simplify markup and improve formatting

--- resources/views/vendor/breadcrumbs/tailwind.blade.php
+++ resources/views/vendor/breadcrumbs/tailwind.blade.php
@@ -1,27 +1,30 @@
 @unless ($breadcrumbs->isEmpty())
-    <nav class="container mx-auto">
-        <ol class="p-4 flex flex-wrap bg-gray-100 text-sm text-gray-800">
-            @foreach ($breadcrumbs as $breadcrumb)
+    <nav>
+        <ol class="p-4 bg-gray-100 text-sm text-gray-800">
+            <x-container class="max-w-7xl flex flex-wrap">
+                @foreach ($breadcrumbs as $breadcrumb)
 
-                @if ($breadcrumb->url && !$loop->last)
-                    <li>
-                        <a href="{{ $breadcrumb->url }}" class="text-blue-600 hover:text-blue-900 hover:underline focus:text-blue-900 focus:underline">
+                    @if ($breadcrumb->url && !$loop->last)
+                        <li>
+                            <a href="{{ $breadcrumb->url }}"
+                               class="text-blue-600 hover:text-blue-900 hover:underline focus:text-blue-900 focus:underline">
+                                {{ $breadcrumb->title }}
+                            </a>
+                        </li>
+                    @else
+                        <li>
                             {{ $breadcrumb->title }}
-                        </a>
-                    </li>
-                @else
-                    <li>
-                        {{ $breadcrumb->title }}
-                    </li>
-                @endif
+                        </li>
+                    @endif
 
-                @unless($loop->last)
-                    <li class="text-gray-500 px-2">
-                        /
-                    </li>
-                @endif
+                    @unless($loop->last)
+                        <li class="text-gray-500 px-2">
+                            /
+                        </li>
+                    @endif
 
-            @endforeach
+                @endforeach
+            </x-container>
         </ol>
     </nav>
 @endunless
