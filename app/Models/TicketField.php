4f957d09908555313694255493b9bf6fa8ba1e25
Liam Seys <liam.seys@gmail.com>
2025-01-29 22:06:56 +0100

    feat: add TicketField model, factory, and migration

---
+++ app/Models/TicketField.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\HelpCenter\FormField;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TicketField extends Model
+{
+    /** @use HasFactory<\Database\Factories\TicketFieldFactory> */
+    use HasFactory, HasUlids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'ticket_id',
+        'form_field_id',
+        'value',
+    ];
+
+    /**
+     * TicketField belongs to a Ticket.
+     *
+     * @return BelongsTo
+     */
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    /**
+     * TicketField belongs to a FormField.
+     *
+     * @return BelongsTo
+     */
+    public function formField()
+    {
+        return $this->belongsTo(FormField::class);
+    }
+}
