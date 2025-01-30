e7703f7c7f3adf318ec1c1345dd580a1dace7763
Liam Seys <liam.seys@gmail.com>
2025-01-30 12:11:51 +0100

    refactor(EditForm): remove unused view file for edit form

--- app/Filament/Clusters/HelpCenter/Resources/FormResource/Pages/EditForm.php
+++ app/Filament/Clusters/HelpCenter/Resources/FormResource/Pages/EditForm.php
@@ -10,8 +10,6 @@ class EditForm extends EditRecord
 {
     protected static string $resource = FormResource::class;
 
-    protected static string $view = 'filament.clusters.help-center.resources.forms.pages.edit-form';
-
     protected function getHeaderActions(): array
     {
         return [
