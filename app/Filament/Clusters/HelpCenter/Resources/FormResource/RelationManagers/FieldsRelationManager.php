5cc95070d7dd8f75478b6500b8afacf80112b9a3
Liam Seys <liam.seys@gmail.com>
2025-01-30 15:05:44 +0100

    feat: add tabbed interface to fields relation manager

--- app/Filament/Clusters/HelpCenter/Resources/FormResource/RelationManagers/FieldsRelationManager.php
+++ app/Filament/Clusters/HelpCenter/Resources/FormResource/RelationManagers/FieldsRelationManager.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Clusters\HelpCenter\Resources\FormResource\RelationManagers;
 
+use App\Enums\HelpCenter\Forms\FormFieldType;
 use Filament\Forms;
+use Filament\Forms\Components\Tabs;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
@@ -16,9 +18,53 @@ class FieldsRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('label')
-                    ->required()
-                    ->maxLength(255),
+                Tabs::make('Tabs')
+                    ->tabs([
+                        Tabs\Tab::make(__('General'))
+                            ->icon('heroicon-o-information-circle')
+                            ->schema([
+                                Forms\Components\Grid::make()
+                                    ->schema([
+                                        Forms\Components\Select::make('type')
+                                            ->label(__('Type'))
+                                            ->options(FormFieldType::class)
+                                            ->searchable()
+                                            ->preload()
+                                            ->required()
+                                            ->live(),
+                                        Forms\Components\TextInput::make('label')
+                                            ->label(__('Label'))
+                                            ->required()
+                                            ->maxLength(255),
+                                        Forms\Components\TextInput::make('description')
+                                            ->label(__('Description'))
+                                            ->columnSpanFull(),
+                                        Forms\Components\Grid::make()
+                                            ->schema([
+                                                Forms\Components\Toggle::make('is_required')
+                                                    ->label(__('Required'))
+                                                    ->default(true)
+                                                    ->required()
+                                                    ->helperText(__('Indicates if this field is required and must be filled out before the form can be submitted.')),
+                                                Forms\Components\Toggle::make('is_visible')
+                                                    ->label(__('Visible'))
+                                                    ->default(true)
+                                                    ->required()
+                                                    ->helperText(__('Visible fields appear on the form, while hidden fields are only accessible to agents.')),
+                                            ]),
+                                    ]),
+                            ]),
+                        Tabs\Tab::make(__('Options'))
+                            ->icon('heroicon-o-list-bullet')
+                            ->schema([
+                                // ...
+                            ]),
+                        Tabs\Tab::make(__('Extra validation'))
+                            ->icon('heroicon-o-variable')
+                            ->schema([
+                                // ...
+                            ]),
+                    ])->columnSpanFull(),
             ]);
     }
 
