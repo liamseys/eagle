7a10822bd67a0f806406c3665fb72518ace2bf24
Liam Seys <liam.seys@gmail.com>
2025-02-05 22:31:54 +0100

    feat(components): add select component with props support

---
+++ resources/views/components/select.blade.php
@@ -0,0 +1,7 @@
+@props(['disabled' => false, 'required' => false])
+
+<select {{ $disabled ? 'disabled' : '' }}
+    {{ $required ? 'required' : '' }}
+    {!! $attributes->merge(['class' => 'border-gray-300 focus:border-primary-500 focus:ring-primary-500 rounded-md shadow-sm disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200']) !!}>
+    {{ $slot }}
+</select>
