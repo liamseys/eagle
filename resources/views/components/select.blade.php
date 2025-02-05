dded34f43c8e7e1b98a43543b5a60caceab31d3a
Liam Seys <liam.seys@gmail.com>
2025-02-05 22:48:05 +0100

    style: update input and select component styles

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -2,6 +2,6 @@
 
 <select {{ $disabled ? 'disabled' : '' }}
     {{ $required ? 'required' : '' }}
-    {!! $attributes->merge(['class' => 'border-gray-300 focus:border-primary-500 focus:ring-primary-500 rounded-md shadow-sm disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200']) !!}>
+    {!! $attributes->merge(['class' => 'text-sm rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200']) !!}>
     {{ $slot }}
 </select>
