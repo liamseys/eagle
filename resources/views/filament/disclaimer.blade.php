7f9380ed5eb5b164bd4a4fbdaa34d0c1e01fb0b4
Liam Seys <liam.seys@gmail.com>
2025-02-08 16:58:00 +0100

    docs: update disclaimer with links for contributions

--- resources/views/filament/disclaimer.blade.php
+++ resources/views/filament/disclaimer.blade.php
@@ -1,8 +1,10 @@
 <div class="py-8 px-6">
     <div class="p-4 bg-gray-100 rounded-lg">
         <p class="text-gray-500 text-xs">
-            This project, Eagle, is free and open source, built by Liam Seys.
-            Consider contributing or sponsoring to support its development.
+            This project, Eagle, is free and open source, built by
+            <a href="https://github.com/liamseys" rel="nofollow noreferrer noopener" target="_blank" class="hover:underline">Liam Seys</a>.
+            Consider <a href="https://github.com/liamseys/eagle" rel="nofollow noreferrer noopener" target="_blank" class="hover:underline">contributing</a> or
+            <a href="https://github.com/sponsors/liamseys" rel="nofollow noreferrer noopener" target="_blank" class="hover:underline">sponsoring</a> to support its development.
         </p>
     </div>
 </div>
