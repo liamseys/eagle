271b4f9d7e38a2a42962c95eead7ada751f3abd7
Liam Seys <liam.seys@gmail.com>
2025-02-10 22:21:37 +0100

    refactor(routes): update command for closing resolved tickets

--- routes/console.php
+++ routes/console.php
@@ -1,11 +1,12 @@
 <?php
 
-use Illuminate\Console\Scheduling\Schedule;
+use App\Console\Commands\CloseResolvedTicketsCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('tickets:close-resolved')->daily();
+Schedule::command(CloseResolvedTicketsCommand::class)->daily();
