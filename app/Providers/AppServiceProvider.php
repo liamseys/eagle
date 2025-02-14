20d8da201a72ee0f80381fac1672a658ee1570ea
Liam Seys <liam.seys@gmail.com>
2025-02-14 13:37:31 +0100

    feat: register GeneralSettings singleton and update button style

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(GeneralSettings::class, function () {
+            return new GeneralSettings;
+        });
     }
 
     /**
