dd13b5b57e21c38c005a5005f471415784fb815b
Liam Seys <liam.seys@gmail.com>
2025-04-15 21:01:41 +0200

    style: remove unused icon class from alert component

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,6 +1,6 @@
 @props(['icon' => null])
 
-<div class="flex items-center p-4 text-sm text-gray-800 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600" role="alert">
+<div class="flex items-center p-4 text-sm text-gray-800 border border-gray-300 rounded-lg bg-gray-50" role="alert">
     @if(isset($icon))
         <x-dynamic-component :component="'heroicon-m-'.$icon" class="shrink-0 inline w-5 h-5 me-2"/>
     @endif
--- resources/views/filament/pages/actions/token-modal.blade.php
+++ resources/views/filament/pages/actions/token-modal.blade.php
@@ -3,5 +3,9 @@
         {{ __('Make sure to copy your personal access token now. You won’t be able to see it again!') }}
     </x-alert>
 
+    <x-input type="text" name="test"
+             id="test" class="w-full"
+             required/>
+
     {{ $test }}
 </div>
