c7094dc11e0b91cdc10c9e6ca764857f33929354
Liam Seys <liam.seys@gmail.com>
2025-04-20 23:28:55 +0200

    feat(TicketResource): add ticket resource and its pages

---
+++ app/Filament/Client/Resources/TicketResource.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Filament\Client\Resources;
+
+use App\Filament\Client\Resources\TicketResource\Pages;
+use App\Models\Scopes\GroupScope;
+use App\Models\Ticket;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class TicketResource extends Resource
+{
+    protected static ?string $model = Ticket::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->withoutGlobalScopes([GroupScope::class]);
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('requester_id')
+                    ->maxLength(26),
+                Forms\Components\TextInput::make('assignee_id')
+                    ->maxLength(26),
+                Forms\Components\TextInput::make('group_id')
+                    ->maxLength(26),
+                Forms\Components\TextInput::make('ticket_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('duplicate_of_ticket_id')
+                    ->maxLength(26),
+                Forms\Components\TextInput::make('subject')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('priority')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('normal'),
+                Forms\Components\TextInput::make('type')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('status')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('open'),
+                Forms\Components\Toggle::make('is_escalated')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('ID')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('requester_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('assignee_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('group_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('ticket_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('duplicate_of_ticket_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('subject')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('priority')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('type')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('is_escalated')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTickets::route('/'),
+            'create' => Pages\CreateTicket::route('/create'),
+            'view' => Pages\ViewTicket::route('/{record}'),
+            'edit' => Pages\EditTicket::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Client/Resources/TicketResource/Pages/CreateTicket.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Client\Resources\TicketResource\Pages;
+
+use App\Filament\Client\Resources\TicketResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTicket extends CreateRecord
+{
+    protected static string $resource = TicketResource::class;
+}
