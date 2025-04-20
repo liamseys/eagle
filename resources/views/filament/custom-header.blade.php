06011c6c5bb448e60b18aa38beb68ee0a8825a60
Liam Seys <liam.seys@gmail.com>
2025-04-20 21:35:01 +0200

    feat(filament): update header title and add description

--- resources/views/filament/custom-header.blade.php
+++ resources/views/filament/custom-header.blade.php
@@ -1,1 +1,2 @@
-<x-hero :title="__('Help Center')"/>
+<x-hero :title="__('Client portal')"
+        :description="__('We\'re here to assist you. Whether you have a general question or need help with something specific, our team is ready to provide quick and satisfying support.')"/>
