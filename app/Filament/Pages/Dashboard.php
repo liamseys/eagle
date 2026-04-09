e0e0a667f93c0b4d9476971d7e28372e572f7f18
Liam Seys <liam.seys@gmail.com>
2026-04-09 09:04:50 +0200

    feat: add section component to filters form in dashboard

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -6,6 +6,7 @@ use Filament\Actions\Action;
 use Filament\Forms\Components\DatePicker;
 use Filament\Pages\Dashboard as BaseDashboard;
 use Filament\Pages\Dashboard\Concerns\HasFiltersForm;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class Dashboard extends BaseDashboard
@@ -16,27 +17,30 @@ class Dashboard extends BaseDashboard
     {
         return $schema
             ->components([
-                DatePicker::make('startDate')
-                    ->label('Start date')
-                    ->native(false)
-                    ->suffixAction(
-                        Action::make('clearStartDate')
-                            ->icon('heroicon-m-x-mark')
-                            ->action(fn ($set) => $set('startDate', null))
-                            ->visible(fn ($state) => filled($state))
-                    )
-                    ->live(),
-                DatePicker::make('endDate')
-                    ->label('End date')
-                    ->native(false)
-                    ->suffixAction(
-                        Action::make('clearEndDate')
-                            ->icon('heroicon-m-x-mark')
-                            ->action(fn ($set) => $set('endDate', null))
-                            ->visible(fn ($state) => filled($state))
-                    )
-                    ->live(),
-            ])
-            ->columns(3);
+                Section::make('Filters')
+                    ->schema([
+                        DatePicker::make('startDate')
+                            ->label('Start date')
+                            ->native(false)
+                            ->suffixAction(
+                                Action::make('clearStartDate')
+                                    ->icon('heroicon-m-x-mark')
+                                    ->action(fn ($set) => $set('startDate', null))
+                                    ->visible(fn ($state) => filled($state))
+                            )
+                            ->live(),
+                        DatePicker::make('endDate')
+                            ->label('End date')
+                            ->native(false)
+                            ->suffixAction(
+                                Action::make('clearEndDate')
+                                    ->icon('heroicon-m-x-mark')
+                                    ->action(fn ($set) => $set('endDate', null))
+                                    ->visible(fn ($state) => filled($state))
+                            )
+                            ->live(),
+                    ])
+                    ->columns(3),
+            ]);
     }
 }
