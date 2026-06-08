51f75deeefbd0806285a63af2a7e7ed5eeb61c20
Liam Seys <liam.seys@gmail.com>
2026-06-08 14:07:10 +0200

    Update build assets, manifest, and widget view

--- resources/views/filament/client/widgets/looking-for-something-else.blade.php
+++ resources/views/filament/client/widgets/looking-for-something-else.blade.php
@@ -1,10 +1,13 @@
 <x-filament-widgets::widget>
-    <div class="flex flex-row items-center gap-2">
-        <x-heroicon-m-information-circle class="h-8 w-8"/>
-        <div>
-            <p class="text-sm font-bold">{{ __('Are you looking for something else?') }}</p>
+    <div class="flex items-start gap-3">
+        <div class="flex size-9 flex-none items-center justify-center rounded-lg bg-gray-950/5 text-gray-700">
+            <x-heroicon-o-information-circle class="size-5"/>
+        </div>
+
+        <div class="space-y-0.5">
+            <p class="text-sm font-semibold text-gray-950">{{ __('Are you looking for something else?') }}</p>
             <p class="text-sm text-gray-500">
-                {!! __('Explore our <a href=":url" class="hover:underline">other help pages</a> for more information.', ['url' => route('index', ['locale' => config('app.locale')])]) !!}
+                {!! __('Explore our <a href=":url" class="font-medium text-gray-700 underline underline-offset-2 hover:text-gray-950">other help pages</a> for more information.', ['url' => route('index', ['locale' => config('app.locale')])]) !!}
             </p>
         </div>
     </div>
