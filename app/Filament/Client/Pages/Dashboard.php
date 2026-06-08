c1f93333a6980aee6fd7c8d1985f8a1dc88741fb
Liam Seys <liam.seys@gmail.com>
2026-06-08 14:01:18 +0200

    feat(Dashboard): add navigation label and title to Dashboard

--- app/Filament/Client/Pages/Dashboard.php
+++ app/Filament/Client/Pages/Dashboard.php
@@ -4,6 +4,10 @@ namespace App\Filament\Client\Pages;
 
 class Dashboard extends \Filament\Pages\Dashboard
 {
+    protected static ?string $navigationLabel = 'Home';
+
+    protected static ?string $title = 'Home';
+
     public function getHeading(): string
     {
         return '';
